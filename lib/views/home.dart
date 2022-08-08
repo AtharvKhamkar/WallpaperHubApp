f32193997b4b534fdb15afae7a654e90b62d4869
Mahesh <maheshkarande1985@gmail.com>
2022-08-08 20:38:53 +0530

    List view added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:wallpaperapp/data/data.dart';
+import 'package:wallpaperapp/model/categoriesModel.dart';
+
 import 'package:wallpaperapp/widget/widget.dart';
 
 class home extends StatefulWidget {
@@ -9,6 +13,13 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  List<CategoriesModel> categories = [];
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,9 +52,42 @@ class _homeState extends State<home> {
               ],
             ),
           ),
-          
+          SizedBox(
+            height: 16,
+          ),
+          ListView.builder(
+              itemCount: categories.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return CategoriesTile(
+                    imgUrl: categories[index].ImageUrl,
+                    title: categories[index].categoriesName);
+              })
         ],
       )),
+    );
+  }
+}
+
+class CategoriesTile extends StatelessWidget {
+  final String? imgUrl, title;
+  CategoriesTile({
+    Key? key,
+    required this.imgUrl,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(children: <Widget>[
+        Container(
+          child: Image.network(imgUrl!),
+        ),
+        Container(
+          child: Text(title!),
+        )
+      ]),
     );
   }
 }
