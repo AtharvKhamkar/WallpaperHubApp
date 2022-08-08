c83123b7066e22d082d70a66970525f25869d3fc
Mahesh <maheshkarande1985@gmail.com>
2022-08-08 17:05:05 +0530

    appbar and searchbar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -12,11 +12,37 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Brandname(),
-        elevation: 1.0,
+        title: brandName(),
+        elevation: 0.0,
       ),
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(30)),
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "search wallpaper",
+                    ),
+                  ),
+                ),
+                Icon(Icons.search),
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-Widget Brandname() {
+Widget brandName() {
   return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
-      Text("Wallpaper",style: TextStyle(color:Colors.black87),),
-      Text("Hub",style: TextStyle(color:Colors.blue),),
-
+      Text(
+        "Wallpaper",
+        style: TextStyle(color: Colors.black87),
+      ),
+      Text(
+        "Hub",
+        style: TextStyle(color: Colors.blue),
+      ),
     ],
   );
 }
